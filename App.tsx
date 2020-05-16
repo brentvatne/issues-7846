8b4ff488628444187fe4f9996171071d93a6d54a
Brent Vatne <brentvatne@gmail.com>
2020-05-16 16:39:20 -0700

    repro

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates';
+
+async function maybeUpdate() {
+  let result = await Updates.checkForUpdateAsync();
+  if (result.isAvailable) {
+    alert('Update is available!');
+  } else {
+    alert('Checked for update, none available');
+  }
+}
 
 export default function App() {
+  useEffect(() => {
+    maybeUpdate();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello there! Nothing to see in the views here.</Text>
     </View>
   );
 }
